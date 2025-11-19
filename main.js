8868a1f45f5122eee3d063284d1845d230ac0584
vikramgaming <pikrampikram25@gmail.com>
2025-11-20 06:39:18 +0700

    ya

--- main.js
+++ main.js
@@ -1,38 +1,6 @@
-const moreTeks = document.getElementById("moreTeks");
-let hasShow = false
-
-function more() {
-    if (moreTeks.textContent !== "...") return;
-    moreTeks.textContent = "";
-    const teks = "tentu saja jadi suka coding. 😉";
-    let i = 0;
-    let adding = setInterval(() => {
-    if (i > teks.length-3) return stop();;
-        moreTeks.textContent += teks[i];
-        i++;
-    }, 25);
-    function stop() {
-        let end = teks.slice(-2);
-        setTimeout(() => {moreTeks.textContent += end}, 1000);
-        clearInterval(adding);
-    }
-    moreTeks.disabled = true;
-}
-function klarifi() {
-    if (!hasShow) {
-        const more = document.getElementById("klarifikasi");
-        hasShow = true
-        more.classList.remove("hidden");
-    };
-}
-
-function ganti(show, hide1, hide2) {
-    const more = document.getElementById("klarifikasi");
+function ganti(show, hide1) {
     const showPage = document.getElementsByClassName(`${show}`);
     const hide1Page = document.getElementsByClassName(`${hide1}`);
-    const hide2Page = document.getElementsByClassName(`${hide2}`);
-    if (!more.classList.contains("hidden")) more.classList.add("hidden");
     showPage[0].classList.remove("hidden");
     hide1Page[0].classList.add("hidden");
-    hide2Page[0].classList.add("hidden");
 }
